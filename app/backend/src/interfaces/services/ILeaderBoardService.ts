2355a1431b2fb76de22ce0af26ceebc1c8ec86ed
Vinicius <xxvi.paris@gmail.com>
2022-10-27 18:25:22 -0300

    fixx GET / leaderboard/home

--- app/backend/src/interfaces/services/ILeaderBoardService.ts
+++ app/backend/src/interfaces/services/ILeaderBoardService.ts
@@ -8,7 +8,7 @@ export interface ILeaderBoard {
   goalsFavor: number;
   goalsOwn: number;
   goalsBalance: number;
-  efficiency?: number;
+  efficiency?: string;
 }
 
 export default interface ILeaderBoardService {
--- app/backend/src/services/LeaderBoardService.ts
+++ app/backend/src/services/LeaderBoardService.ts
@@ -4,7 +4,7 @@ import ILeaderBoardService, { ILeaderBoard } from '../interfaces/services/ILeade
 
 export default class LeaderBoardService implements ILeaderBoardService {
   public getLeaderBoardHome = async (): Promise<ILeaderBoard[]> => {
-    const leaderBoard = await sequelize.query(leaderBoardHomeQ);
+    const [leaderBoard] = await sequelize.query(leaderBoardHomeQ);
     return leaderBoard as ILeaderBoard[];
   };
 }
